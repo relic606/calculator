2ecbd39e957e21d3eb0753f50b28daf3302a098a
Andy Anderson <relic606@yahoo.com>
2022-02-19 14:31:55 -0700

    Addition implementation working

--- calculator/calc.js
+++ calculator/calc.js
@@ -14,8 +14,29 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
 
+	if(mathSymbol === '+'){
+		console.log(num1 + num2)
+	}
+	// if(mathSymbol === '-'){
+	// 	console.log(num1 - num2)
+	// }
+	// if(mathSymbol === '*'){
+	// 	console.log(num1 * num2)
+	// }
+	// if(mathSymbol === '/'){
+	// 	console.log(num1 / num2)
+	// }
+	// if(mathSymbol === 'sqrt'){
+	// 	console.log()
+	// }
 	// This line closes the connection to the command line interface.
 	reader.close()
 
 });
+
+
+
